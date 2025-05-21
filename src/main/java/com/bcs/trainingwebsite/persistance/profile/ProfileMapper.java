dba5de38da933a252e55a19f42d0a07176303942
uvema <uvemand@gmail.com>
2025-05-21 10:18:06 +0300

    treeneri lisamine korras

--- src/main/java/com/bcs/trainingwebsite/persistance/profile/ProfileMapper.java
+++ src/main/java/com/bcs/trainingwebsite/persistance/profile/ProfileMapper.java
@@ -4,17 +4,8 @@ import com.bcs.trainingwebsite.controller.registration.dto.CoachProfile;
 import com.bcs.trainingwebsite.controller.registration.dto.CustomerProfile;
 import org.mapstruct.*;
 
-import java.time.LocalDate;
-
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProfileMapper {
-
-//    private String firstName;
-//    private String lastName;
-//    private String email;
-//    private String phoneNumber;
-//    private LocalDate dateOfBirth;
-//    private String gender;
 
     @Mapping(source = "firstName", target = "firstName")
     @Mapping(source = "lastName", target = "lastName")
@@ -25,8 +16,8 @@ public interface ProfileMapper {
 
 
     @InheritConfiguration(name = "toProfile")
-    @Mapping(source = "phoneNumber",target = "phone")
-    @Mapping(source = "description",target = "description")
+    @Mapping(source = "phoneNumber", target = "phone")
+    @Mapping(source = "description", target = "description")
     Profile toCoachProfile(CoachProfile coachProfile);
 
 
